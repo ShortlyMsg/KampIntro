d5ad8130e9412f42913f00813afe22e6be02ce59
M.Said Gürbüz <msg_256@hotmail.com>
2021-01-16 23:48:03 +0300

    16 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama :)))
+            //değer tutucu - alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double oran = 1.4;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //Naming Convention
+        //syntax 
+        public void  Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urun.Adi);
+            //
+            //
+            //
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
